0698532ba8879bea5ad00358bc71166fb65044a9
Kenneth Lamb <55679262+slurrps-mcgee@users.noreply.github.com>
2021-09-09 01:43:28 -0400

    Update to css

--- js/main.js
+++ js/main.js
@@ -23,11 +23,10 @@ marker.setLatLng([42.361145, -71.057083]);
 
 //Adds a tile layer to the map using the mapbox api and adds attributions
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery ï¿½ <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
-
