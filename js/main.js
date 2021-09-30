b257f6687affdad41b11aac7e1c2827dc8dd7cdb
Kenneth Lamb <55679262+slurrps-mcgee@users.noreply.github.com>
2021-09-30 00:25:08 -0400

    Fixed Map
    Fixed the issue with the map on the bottom of the page showing the location of the business.

--- js/main.js
+++ js/main.js
@@ -3,30 +3,22 @@
 //Map variable set to an instance of L.map grabbing the ID of the map container
 //Also turning off all control to make a static map
 //Then setting the view to the location of the businuess
-var mymap = L.map('mapid', { dragging: false,
-    touchZoom: false,
-    scrollWheelZoom: false,
-    doubleClickZoom: false,
-    boxZoom: false,
-    tap: false,
-    keyboard: false,
-    zoomControl: false,
-    attributionControl: false,
-    prefix: false}).setView([42.361145, -71.057083], 18);
+var mymap = L.map('mapid',
+{
+  zoomControl: false
+}).setView([42.37330, -71.06225], 20);
 
 //Create a constant marker to put on the map with the incomming latitude and longitude
 //Add to the map
-const marker = L.marker([42.361145, -71.057083]).addTo(mymap);
+const marker = L.marker([42.37330, -71.06225]).addTo(mymap);
 
-//set marker latitude and longitude
-marker.setLatLng([42.361145, -71.057083]);
-
-//Adds a tile layer to the map using the mapbox api and adds attributions
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery � <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+//Add the title layer to the map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/outdoors-v11',
+  tileSize: 512,
+  zoomOffset: -1
 }).addTo(mymap);
