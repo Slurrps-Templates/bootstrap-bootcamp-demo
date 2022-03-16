ffd6f19a1e0667d76fd4908032aa2e47ca5d8691
Kenneth Lamb <55679262+slurrps-mcgee@users.noreply.github.com>
2022-03-16 11:50:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,8 +16,6 @@ const marker = L.marker([42.37330, -71.06225]).addTo(mymap);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
 {
   maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   id: 'mapbox/outdoors-v11',
   tileSize: 512,
   zoomOffset: -1
